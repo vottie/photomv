77dac48bdf27ce4d7992c1751d5d13ebef41aff3
vottie <tsuboi23@hotmail.com>
2019-09-09 02:25:06 +0900

    Add singleton.

---
+++ WpfApp1/WpfApp1/PhotoMVSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    class PhotoMVSingleton
+    {
+        private static PhotoMVSingleton _singleton = new PhotoMVSingleton();
+
+        public static PhotoMVSingleton GetInstance()
+        {
+            return _singleton;
+        }
+
+        private PhotoMVSingleton()
+        {
+
+        }
+    }
+}
